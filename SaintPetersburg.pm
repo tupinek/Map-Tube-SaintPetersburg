523b00df351af0ab59c41015531f28f09bd3295d
Michal Špaček <tupinek@gmail.com>
2015-11-05 11:29:51 +0100

    Add EXAMPLE5 section to doc.

--- SaintPetersburg.pm
+++ SaintPetersburg.pm
@@ -221,6 +221,48 @@ For more information about Saint Petersburg Map, click L<here|https://en.wikiped
  # Правобережная линия
  # Фрунзенско-Приморская линия
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(decode_utf8 encode_utf8);
+ use Map::Tube::SaintPetersburg;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = decode_utf8($ARGV[0]);
+
+ # Object.
+ my $obj = Map::Tube::SaintPetersburg->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Правобережная линия' argument.
+ # Спасская
+ # Достоевская
+ # Лиговский проспект
+ # Площадь Александра Невского-2
+ # Новочеркасская
+ # Ладожская
+ # Проспект Большевиков
+ # Улица Дыбенко
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
